4891125dc33a12adcd8da714938e21541303cfbc
abhi221197 <90088182+abhi221197@users.noreply.github.com>
2021-10-24 11:37:16 +0530

    init

---
+++ ASCI.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main() 
+{
+	char ch;
+	printf("\n enter any charcter");
+	scanf("%c",&ch);
+	if ((ch=='a')||(ch=='e')||(ch=='i')||(ch=='o')||(ch=='u'))
+	{
+		printf("\n %c is the small vovel",ch);
+	}
+	else if ((ch=='A')||(ch=='E')||(ch=='I')||(ch=='O')||(ch=='U'))
+	{
+		printf("\n %c is the capital vovel",ch);
+		
+	}
+	 else if ((ch>=65)&&(ch<=90))
+	{
+		printf("its captial character",ch);
+		
+		}	0
+	else if ((ch>=48)&&(ch<=57))
+
+{
+	printf(" its a number",ch);
+	}	
+	
+	else {
+		printf("\n its an another chacarter",ch);
+		
+	}
+	return 0;
+}
